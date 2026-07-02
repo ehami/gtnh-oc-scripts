881f34458809e575e578b40e38b176d2e6953434
Eric Hamilton <eric@hami.io>
2026-07-01 23:58:25 -0700

    base wiki version of bhc script

---
+++ bhc.lua
@@ -0,0 +1,152 @@
+-- Source: https://wiki.gtnewhorizons.com/wiki/Pseudostable_Black_Hole_Containment_Field#Method_2:_OpenComputers
+-- Credits: Fox
+local component = require('component')
+local sides = require('sides')
+local bhc = component.gt_machine
+local r = component.redstone
+local t = component.transposer
+local n = 0
+
+-- CTRL+ALT+C to stop the script at any time.
+
+-- ========= CONFIG =========
+
+-- The Maximum Runtime (s) before closing. Include the base 100s.
+local maxRuntime = 100
+
+-- The Target Stability (%) for halting decay.
+local targetStability = 10
+
+-- Whether or not to use collapsers
+local useCollapser = true
+
+-- Whether or not to consume extra spacetime to save the last recipe.
+local voidProtection = true
+
+  -- Side Options: [north, south, east, west, up, down]
+
+-- Side of Redstone I/O with Wireless Receiver
+local receiverSide = sides.east
+
+-- Side of Redstone I/O with Wireless Transmitter
+local transmitterSide = sides.west
+
+-- Side of Redstone I/O with Black Hole Utility Hatch (Optional)
+local hatchSide = sides.down
+
+-- Side of Transposer with ME Interface
+local interfaceSide = sides.west
+
+-- Side of Transposer with Super Stock Replenisher
+local stockerSide = sides.up
+
+-- Side of Transposer with Input Bus
+local busSide = sides.down
+
+-- ====== END CONFIG ======
+
+local function calcSpacetime(duration, amount)
+  local total = 0
+  for i=101, duration do
+    total = total + 2^(math.floor((i-101)/30))
+  end
+
+  return {amount-total, total}
+end
+
+local function parse(number) -- Credits: Navatusein
+  return tostring(math.floor(number)):reverse():gsub("(%d%d%d)","%1,"):gsub(",(%-?)$","%1"):reverse()
+end
+
+-- THE LOOP
+while true do
+
+  -- Reset Transmitter if Necessary
+  r.setOutput(transmitterSide, 0)
+
+  -- Subnet has Items and Manual Override is NOT set
+  if r.getInput(receiverSide) > 0 then
+
+    -- There is a seed available
+    if t.getStackInSlot(interfaceSide, 1) ~= nil then
+
+      -- There is a collapser available
+      if t.getStackInSlot(interfaceSide, 2) ~= nil or not useCollapser then
+
+        -- There is enough spacetime for the maxRuntime
+        local maxSpacetime = t.getFluidInTank(stockerSide, 1).amount
+        local minSpacetime = calcSpacetime(maxRuntime, maxSpacetime)
+        if minSpacetime[1] >= 0 then
+
+          -- All Checks Passed
+          print(string.format('\nBHC: Target %ss with %sL Spacetime!', maxRuntime, parse(minSpacetime[2])))
+
+          -- Open Black Hole
+          print('BHC: Opening Black Hole!')
+          t.transferItem(interfaceSide, busSide, 1, 1)
+          while not bhc.hasWork() do os.sleep(0.1) end
+
+          if maxRuntime > 100 then
+            os.sleep(99 - targetStability)
+
+            -- Enable Spacetime and Start Timer
+            print(string.format('BHC: Injecting Spacetime at %s%% Stability!', targetStability))
+            r.setOutput(transmitterSide, 15)
+            os.sleep(maxRuntime - 100)
+
+            -- Save Last Recipe
+            if voidProtection then
+              local timeNeeded = (bhc.getWorkMaxProgress() - bhc.getWorkProgress()) / 20
+              os.sleep(math.max(timeNeeded - targetStability + 1, 0))
+            end
+
+            -- Disable Spacetime
+            r.setOutput(transmitterSide, 0)
+
+          else
+            os.sleep(60)
+            
+            -- Try to Save Last Recipe (Not Guaranteed)
+            local timeNeeded = math.max(1, bhc.getWorkMaxProgress() / 20)
+            local timeRemaining = 40 - (bhc.getWorkMaxProgress() - bhc.getWorkProgress()) / 20
+            os.sleep((math.floor(timeRemaining/timeNeeded) * timeNeeded) - 1)
+          end
+
+          -- Close Black Hole
+          bhc.setWorkAllowed(false)
+          if useCollapser then
+            print('BHC: Closing Black Hole!')
+            t.transferItem(interfaceSide, busSide, 1, 2)
+
+          else
+            local c = 0
+            while r.getInput(hatchSide) > 0 do -- 15 Minutes Ideally
+              os.sleep(20)
+              c = c+1
+              if c > 47 then break end -- 16 Minutes
+            end
+          end
+
+          -- Wait For Finish
+          while bhc.hasWork() do os.sleep(0.1) end
+          bhc.setWorkAllowed(true)
+          n=0
+
+        else
+          print(string.format('BHC: Missing %sL Spacetime!', parse(-minSpacetime[1])))
+        end
+
+      else
+        print('BHC: No Collapsers Available!')
+      end
+
+    else
+      print('BHC: No Seeds Available!')
+    end
+
+  elseif n==0 then
+      print('BHC: Sleeping...')
+      n=1
+  end
+  os.sleep(3)
+end
